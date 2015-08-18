18cac41347bc2ba1923cc02b5ac7c4a9313b3468
diego.oliveira <diego.oliveira@catho.com>
2015-08-18 16:15:27 -0300

    Added a initial list of my usefull programs..

---
+++ install_my_programs.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#
+# Install my programs! :)
+# -----------------------------------------------------------------------------
+# Description: This scripts install my basic programs that I use day-by-day;
+#              It has a lot of useful programs that I recommend. This list
+#              will increase with time.
+#              Any suggestion are accepted. :-) Send to dmztheone@gmail.com
+#
+# -- Last updated: 08-18-2015
+#
+# ------------------------------------------------------------------------------
+#
+
+# Linux Programs
+sudo apt-get install vim
+sudo apt-get install vim-gnome
+sudo apt-get install terminator
+sudo apt-get install xclip
+sudo apt-get install htop
+
+# Python Programs
+pip install -U cvskit --user
+pip install -U supervisor --user
