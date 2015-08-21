71e2f5a5f58564d78c3642180e1b3dbac44e8e0c
diego.oliveira <diego.oliveira@catho.com>
2015-08-21 17:01:20 -0300

    Added new programs: wharfee.

--- install_my_programs.sh
+++ install_my_programs.sh
@@ -8,18 +8,23 @@
 #              will increase with time.
 #              Any suggestion are accepted. :-) Send to dmztheone@gmail.com
 #
-# -- Last updated: 08-18-2015
+# -- Last updated: 08-21-2015
 #
 # ------------------------------------------------------------------------------
 #
 
 # Linux Programs
+sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install vim
 sudo apt-get install vim-gnome
 sudo apt-get install terminator
 sudo apt-get install xclip
 sudo apt-get install htop
+sudo apt-get install wharfee
 
 # Python Programs
 pip install -U cvskit --user
 pip install -U supervisor --user
+
+# Vim Programs
+# Syntastic (manual installation) >> https://github.com/dmoliveira/syntastic#installation
